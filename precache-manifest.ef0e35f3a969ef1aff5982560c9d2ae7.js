5d383d91aed94b23c0c9e885b2eb2c98cf7cb1ab
Mikus <mikusmeikulis@gmail.com>
2021-01-15 18:02:57 +0200

    deploy

---
+++ precache-manifest.ef0e35f3a969ef1aff5982560c9d2ae7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d58cce34955cbcc37bf",
+    "url": "/RPSLS/css/app.4b9c5982.css"
+  },
+  {
+    "revision": "01737cedd53a393bf4c7",
+    "url": "/RPSLS/css/chunk-vendors.b0b28440.css"
+  },
+  {
+    "revision": "cf01eddbc8f52fa1ea71f86951c13314",
+    "url": "/RPSLS/img/computer.cf01eddb.png"
+  },
+  {
+    "revision": "90076dd6fddefabd37fcba222b380b62",
+    "url": "/RPSLS/img/lizard.90076dd6.png"
+  },
+  {
+    "revision": "8d4bc302cb6157537beba80e5e725cc8",
+    "url": "/RPSLS/img/paper.8d4bc302.png"
+  },
+  {
+    "revision": "0243ed4a905b50cae21a04bf2c980a4d",
+    "url": "/RPSLS/img/rock.0243ed4a.png"
+  },
+  {
+    "revision": "84b86c0134636d365a3939b3eddb13f2",
+    "url": "/RPSLS/img/rpsls.84b86c01.png"
+  },
+  {
+    "revision": "7bfef90e0f3e2e4dac5649c6f3e50b0f",
+    "url": "/RPSLS/img/scissors.7bfef90e.png"
+  },
+  {
+    "revision": "51a13c3e2706a344122a34f95abee7ff",
+    "url": "/RPSLS/img/spock.51a13c3e.png"
+  },
+  {
+    "revision": "74f5411aef386685f9907e4f009c7fe4",
+    "url": "/RPSLS/index.html"
+  },
+  {
+    "revision": "8d58cce34955cbcc37bf",
+    "url": "/RPSLS/js/app.bf36870e.js"
+  },
+  {
+    "revision": "01737cedd53a393bf4c7",
+    "url": "/RPSLS/js/chunk-vendors.d3c87e3d.js"
+  },
+  {
+    "revision": "27a30e9a98bef9e1d276d4aff6e75e27",
+    "url": "/RPSLS/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/RPSLS/robots.txt"
+  }
+]);
